705cd92921b380e57efd47f434f58ed10ad4fed1
araneoAnicet <wormshoodproduction@gmail.com>
2019-05-05 02:06:33 +0200

    add bootstrap

--- objects_declaration.py
+++ objects_declaration.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
+Bootstrap(app)
 db = SQLAlchemy(app)
+
 
 if __name__ == '__main__':
     print('It works!')
