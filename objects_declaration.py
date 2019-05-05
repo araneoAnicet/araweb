2df4acd694f798efdee694c7aea3fea107ed024a
araneoAnicet <wormshoodproduction@gmail.com>
2019-05-05 02:03:46 +0200

    add CORS

--- objects_declaration.py
+++ objects_declaration.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
 db = SQLAlchemy(app)
 
 if __name__ == '__main__':
